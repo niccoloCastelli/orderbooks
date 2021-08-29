bbfdd4533b685fb215451b65f3efca87b1c85aac
niccolo_castelli <nicco.castelli@gmail.com>
2021-08-29 22:12:17 +0200

    - CLI commands cleanup

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -24,7 +9,6 @@ import (
 	"github.com/spf13/cobra"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
@@ -32,18 +16,12 @@ var cfgFile string
 var logLevel = int8(zerolog.InfoLevel)
 var pairs []common.Pair
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "orderbooks",
 	Short: "Downloader orderbooks",
 	Long:  `Downloader orderbooks`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Error().Err(err).Send()
@@ -54,13 +32,9 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// Global flags
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.json", "config file (default is ./config.json)")
 	rootCmd.PersistentFlags().Int8Var(&logLevel, "log_level", int8(zerolog.InfoLevel), "-1(trace)... 5(panic)")
-
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -79,15 +53,9 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			os.Exit(1)
-		}
-
 		// Search config in home directory with name ".orderbooks" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".orderbooks")
+		viper.AddConfigPath("./")
+		viper.SetConfigName("config.json")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
--- cmd/run.go
+++ cmd/run.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -31,7 +16,6 @@ import (
 	"time"
 )
 
-// runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Start order books scraper",
@@ -62,20 +46,9 @@ var runCmd = &cobra.Command{
 		<-exitChan
 		cancelCtx()
 		_ = sw.Close()
-		//fmt.Println("snapshot",snapshot.Timestamp.String(), len(snapshot.Orders), snapshot.Orders)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
